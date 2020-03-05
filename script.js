691cd4bcd44917912d32bb212aaa02eb91792c67
scadler <scadler@s.sfusd.edu>
2020-03-05 19:05:01 +0000

    added blue center particle with correct physics

--- script.js
+++ script.js
@@ -22,31 +22,45 @@ const status = {
     collision : false,
     touchingWall : false,
 }
-const right = {
-    x : canvas.width/3,
-    y : canvas.height/2,
-    radius : 19.54,
-    vi : 0,
-    v : 0.5,
-    m: 3,
-    color : "White",
-}
 const left = {
-    x : 2*canvas.width/3,
+    x : canvas.width/4,
     y : canvas.height/2,
     radius : 11.28,
     vi : 0,
-    v : -0.05,
-    m: 1,
+    v : 0.5,
+    m: 3,
     color : "Red",
 }
+
+const center = {
+    x : canvas.width/2,
+    y : canvas.height/2,
+    radius : 15,
+    vi : 0,
+    v : 0.1,
+    m: 2,
+    color : "Blue",
+}
+const right = {
+    x : 3*canvas.width/4,
+    y : canvas.height/2,
+    radius : 19.54,
+    vi : 0,
+    v : -0.05,
+    m: 1,
+    color : "White",
+}
+
 function updateElastic(){
     right.vi = right.v
     left.vi = left.v
+    center.vi = center.v
     right.x += right.v;
     left.x += left.v;
+    center.x += center.v
     drawText("Red:     m: "+right.m.toFixed(2)+" kg, v: "+right.v.toFixed(3)+" m/s, KE: "+(1/2*right.m*(right.v*right.v)).toFixed(3)+" N", 0, 10, "White");
     drawText("White:   m: "+left.m.toFixed(2)+" kg, v: "+left.v.toFixed(3)+" m/s, KE: "+(1/2*left.m*(left.v*left.v)).toFixed(3)+" N", 0, 20, "White");
+    drawText("Blue   m: "+center.m.toFixed(2)+" kg, v: "+center.v.toFixed(3)+" m/s, KE: "+(1/2*center.m*(center.v*center.v)).toFixed(3)+" N", 0, 30, "White");
     if( right.x + right.radius > canvas.width || right.x - right.radius < 0){
         right.v = - right.v;
         if(right.x + right.radius > canvas.width){
@@ -59,62 +73,80 @@ function updateElastic(){
             left.x = canvas.width - left.radius;
         }
     }
-    let closeX = Math.sqrt((right.x - left.x)*(right.x - left.x))
-    if(closeX <= (right.radius+left.radius)){
+    if( center.x + center.radius > canvas.width || center.x - center.radius < 0){
+        center.v = - center.v;
+        if(center.x + center.radius > canvas.width){
+            center.x = canvas.width - center.radius;
+        }
+    }
+    let closeRL = Math.sqrt((right.x - left.x)*(right.x - left.x))
+    if(closeRL <= (right.radius+left.radius)){
         right.v = (((right.vi*(right.m - left.m))/(right.m + left.m)) + (((2*left.m)/(right.m + left.m))*left.vi))
         left.v = (((right.vi*(2*right.m))/(right.m + left.m)) + (((left.m - right.m)/(right.m + left.m))*left.vi))
     }
-}
-function updateInelastic(){
-    right.vi = right.v
-    left.vi = left.v
-    right.x += right.v;
-    left.x += left.v;
-    if(right.x - left.x)
-    drawText("Red:     m: "+right.m.toFixed(2)+" kg, v: "+right.v.toFixed(3)+" m/s, KE: "+(1/2*right.m*(right.v*right.v)).toFixed(3)+" N", 0, 10, "White");
-    drawText("White:   m: "+left.m.toFixed(2)+" kg, v: "+left.v.toFixed(3)+" m/s, KE: "+(1/2*left.m*(left.v*left.v)).toFixed(3)+" N", 0, 20, "White");
-    if(status.collision === false){
-    if( right.x + right.radius > canvas.width || right.x - right.radius < 0){
-        right.v = - right.v;
-        if(right.x + right.radius > canvas.width){
-            right.x = canvas.width - right.radius;
-        }
+    let closeCL = Math.sqrt((center.x - left.x)*(center.x - left.x))
+    if(closeCL <= (center.radius+left.radius)){
+        center.v = (((center.vi*(center.m - left.m))/(center.m + left.m)) + (((2*left.m)/(center.m + left.m))*left.vi))
+        left.v = (((center.vi*(2*center.m))/(center.m + left.m)) + (((left.m - center.m)/(center.m + left.m))*left.vi))
     }
-    if( left.x + left.radius > canvas.width || left.x - left.radius < 0){
-        left.v = - left.v;
-        if(left.x + left.radius > canvas.width){
-            left.x = canvas.width - left.radius;
-        }
-    }
-    let closeX = Math.sqrt((right.x - left.x)*(right.x - left.x))
-    if(closeX <= (right.radius+left.radius)){
-        status.collision = true;
-        right.v = ((right.m*right.v)+(left.m*left.v))/(right.m*left.m)
-        left.v = right.v
+    let closeCR = Math.sqrt((center.x - right.x)*(center.x - right.x))
+    if(closeCR <= (center.radius+right.radius)){
+        center.v = (((center.vi*(center.m - right.m))/(center.m + right.m)) + (((2*right.m)/(center.m + right.m))*right.vi))
+        right.v = (((center.vi*(2*center.m))/(center.m + right.m)) + (((right.m - center.m)/(center.m + right.m))*right.vi))
     }
 }
-    else{
-        if(right.x - left.x < right.radius + left.radius){
-            right.x = left.x + right.radius + left.radius
-        }
-        if( right.x + right.radius > canvas.width || right.x - right.radius < 0 || eft.x + left.radius > canvas.width || left.x - left.radius < 0){
-            left.v = -left.v
-            right.v = - right.v
-            if(right.x + right.radius > canvas.width){
-                right.x = canvas.width - right.radius;
-                left.x = left.x - right.radius;
-            }
-            if(left.x + left.radius > canvas.width){
-                left.x = canvas.width - left.radius;
-                right.x = right.x - left.radius;
-         }
-    }
-}
-}
+// function updateInelastic(){
+//     right.vi = right.v
+//     left.vi = left.v
+//     right.x += right.v;
+//     left.x += left.v;
+//     if(right.x - left.x)
+//     drawText("Red:     m: "+right.m.toFixed(2)+" kg, v: "+right.v.toFixed(3)+" m/s, KE: "+(1/2*right.m*(right.v*right.v)).toFixed(3)+" N", 0, 10, "White");
+//     drawText("White:   m: "+left.m.toFixed(2)+" kg, v: "+left.v.toFixed(3)+" m/s, KE: "+(1/2*left.m*(left.v*left.v)).toFixed(3)+" N", 0, 20, "White");
+//     drawText("Blue   m: "+center.m.toFixed(2)+" kg, v: "+center.v.toFixed(3)+" m/s, KE: "+(1/2*center.m*(center.v*center.v)).toFixed(3)+" N", 0, 30, "White");
+//     if(status.collision === false){
+//     if( right.x + right.radius > canvas.width || right.x - right.radius < 0){
+//         right.v = - right.v;
+//         if(right.x + right.radius > canvas.width){
+//             right.x = canvas.width - right.radius;
+//         }
+//     }
+//     if( left.x + left.radius > canvas.width || left.x - left.radius < 0){
+//         left.v = - left.v;
+//         if(left.x + left.radius > canvas.width){
+//             left.x = canvas.width - left.radius;
+//         }
+//     }
+//     let closeX = Math.sqrt((right.x - left.x)*(right.x - left.x))
+//     if(closeX <= (right.radius+left.radius)){
+//         status.collision = true;
+//         right.v = ((right.m*right.v)+(left.m*left.v))/(right.m*left.m)
+//         left.v = right.v
+//     }
+// }
+//     else{
+//         if(right.x - left.x < right.radius + left.radius){
+//             right.x = left.x + right.radius + left.radius
+//         }
+//         if( right.x + right.radius > canvas.width || right.x - right.radius < 0 || eft.x + left.radius > canvas.width || left.x - left.radius < 0){
+//             left.v = -left.v
+//             right.v = - right.v
+//             if(right.x + right.radius > canvas.width){
+//                 right.x = canvas.width - right.radius;
+//                 left.x = left.x - right.radius;
+//             }
+//             if(left.x + left.radius > canvas.width){
+//                 left.x = canvas.width - left.radius;
+//                 right.x = right.x - left.radius;
+//          }
+//     }
+// }
+// }
 function render(){
 drawRect(0, 0, canvas.width, canvas.height, "black");
 drawCircle(left.x, left.y, left.radius, left.color)
 drawCircle(right.x, right.y, right.radius, right.color)
+drawCircle(center.x, center.y, center.radius, center.color)
 $("#redVRangeOutput").text($("#redVRange").val())
 $("#whiteVRangeOutput").text($("#whiteVRange").val())
 $("#redMRangeOutput").text($("#redMRange").val())
@@ -122,17 +154,13 @@ $("#whiteMRangeOutput").text($("#whiteMRange").val())
 }
 function game(){
     render();
-    // if(status.elastic === true){
     updateElastic();
-    // }
-    // else{
-    //     updateInelastic();
-    // }
 }
 setInterval(game);
 $("#restart").click(function(){
-    right.x = canvas.width/3
-    left.x = 2*canvas.width/3
+    right.x = 3*canvas.width/4
+    center.x = canvas.width/2
+    left.x = canvas.width/4
     right.v = Number($("#whiteVRangeOutput").text())
     left.v = Number($("#redVRangeOutput").text())
     right.m = Number($("#whiteMRangeOutput").text())

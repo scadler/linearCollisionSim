18688de91ed29a63ec20f33ccb82801a6c0464d0
scadler <scadler@s.sfusd.edu>
2020-04-07 21:49:02 +0000

    reverted back to commit f0af5e3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const canvas = document.getElementById("plane");
 const context = canvas.getContext("2d");
 //comment
@@ -12,36 +13,16 @@ const status = {
     passCounter: 0,
     blueMultiplierCheck : 0,
     blueMultiplier : 0,
-    CORCheck : 1,
-    COR : 0,
-    CR : false,
-    RL : false,
-    CL : false,
-    CRL : false,
-    order : [],
-    i : 0,
 }
-function drawCircle(x, y, r, color, exist){
-    if(exist === false){
+function drawCircle(x, y, r, color){
     context.fillStyle = color;
     context.beginPath();
     context.arc(x, y, r, 0, Math.PI*2, false);
     context.closePath();
     context.fill();
-    }
-}
-function drawCircleCombined(x, y, r, color){
-    if(combined.draw === true){
-    context.fillStyle = color;
-    context.beginPath();
-    context.arc(x, y, r, 0, Math.PI*2, false);
-    context.closePath();
-    context.fill();
-    console.log(color)
-    }
 }
 function drawCircleBlue(x, y, r, color, check){
-    if(check === true && blue.combined === false){
+    if(check === true){
     context.fillStyle = color;
     context.beginPath();
     context.arc(x, y, r, 0, Math.PI*2, false);
@@ -102,27 +83,15 @@ function checkXCoorSliders(){
         status.xCoorClose = false;
     }
 }
-const combined = {
-    draw : false,
-    x : canvas.width/4,
-    y : canvas.height/2,
-    radius : 10,
-    vi : 0,
-    v : 0,
-    m: 1,
-    color : "#cccccc",
-    status : false,
-}
 const left = {
     x : canvas.width/4,
     y : canvas.height/2,
-    radius : 11.28,
+    radius : 19.54,
     vi : 0,
     v : 0.5,
     m: 3,
-    color : "#FF0000",
+    color : "Red",
     a : 0,
-    combined: false,
 }
 const center = {
     x : canvas.width/2,
@@ -131,75 +100,18 @@ const center = {
     vi : 0,
     v : 0,
     m: 5,
-    color : "#0000FF",
+    color : "Blue",
     a : 0,
-    combined: false,
 }
 const right = {
     x : 3*canvas.width/4,
     y : canvas.height/2,
-    radius : 19.54,
+    radius : 11.28,
     vi : 0,
     v : -0.05,
     m: 1,
-    color : "#FFFFFF",
+    color : "White",
     a : 0,
-    combined: false,
-}
-
-function combinedParticle(c1,c2,m1,m2,v,x1,x2){
-    if(combined.status === true && status.CRL === false){
-        c1 = c1.substring(1)
-        c2 = c2.substring(1)
-        m3 = m1+m2
-        c1R = parseInt( c1.substring(0,2), 16)
-        console.log(c1R)
-        c1G = parseInt( c1.substring(2,4), 16)
-        console.log(c1G)
-        c1B = parseInt( c1.substring(4,6), 16)
-        console.log(c1B)
-        c2R = parseInt( c2.substring(0,2), 16)
-        console.log(c2R)
-        c2G = parseInt( c2.substring(2,4), 16)
-        console.log(c2G)
-        c2B = parseInt( c2.substring(4,6), 16)
-        console.log(c2B)
-        c3R = Math.floor(Math.sqrt(0.5*((c1R * c1R) + (c2R * c2R))))
-        c3R = (c3R.length < 2) ? "0"+c3R : c3R
-        c3G = Math.floor(Math.sqrt(0.5*((c1G * c1G) + (c2G * c2G))))
-        c3G = (c3G.length < 2) ? "0"+c3G : c3G
-        c3B = Math.floor(Math.sqrt(0.5*((c1B * c1B) + (c2B * c1B))))
-        c3B = (c3B.length < 2) ? "0"+c3B : c3B
-        console.log(c3R)
-        console.log(c3G)
-        console.log(c3B)
-        c3 = "#"+c3R.toString(16)+c3G.toString(16)+c3B.toString(16)
-        console.log(c3)
-        x3 = (x1 * (m1/m3)) + (x2 * (m2/m3))
-        combined.color = c3
-        combined.v = v
-        combined.radius = 20*Math.sqrt(Number(m3)/Math.PI)+1
-        combined.x = x3
-        combined.draw = true
-    }
-}
-function collision(a,b){
-    a.v = ( ( ( a.m * a.vi ) + ( b.m * b.vi ) + ( b.m * status.COR * ( b.vi - a.vi ) ) ) / ( a.m + b.m) )
-    b.v = ( ( ( b.m * b.vi ) + ( a.m * a.vi ) + ( a.m * status.COR * ( a.vi - b.vi ) ) ) / ( a.m + b.m) )
-}
-function collisionCOR(a,b,c){
-    a.v = ( ( ( a.m * a.vi ) + ( b * b.vi ) + ( b * status.COR * ( b.vi - a.vi ) ) ) / ( a.m + b) )
-    b.v = ( ( ( b * b.vi ) + ( a.m * a.vi ) + ( a.m * status.COR * ( a.vi - b.vi ) ) ) / ( a.m + b) )
-}
-function overlap(a,b){
-    if(Math.abs(a.x-b.x)< a.radius+b.radius){
-        if(a.x > b.x){
-            b.x = a.x - a.radius - b.radius
-        }
-        else{
-             a.x = b.x - b.radius - a.radius
-        }
-    }
 }
 function updateText(){
     $("#redVRangeOutput").text($("#redVRange").val())
@@ -209,7 +121,6 @@ function updateText(){
     $("#whiteVRangeOutput").text($("#whiteVRange").val())
     $("#whiteMRangeOutput").text($("#whiteMRange").val())
     $("#frictionRangeOutput").text($("#frictionRange").val())
-    $("#CORRangeOutput").text($("#CORRange").val())
     $("#redMOutput").text(left.m.toFixed(2))
     $("#redVOutput").text(left.v.toFixed(3))
     $("#redKEOutput").text((0.5*left.m*(left.v*left.v)).toFixed(4))
@@ -217,78 +128,15 @@ function updateText(){
     $("#whiteMOutput").text(right.m.toFixed(2))
     $("#whiteVOutput").text(right.v.toFixed(3))
     $("#whiteKEOutput").text((0.5*right.m*(right.v*right.v)).toFixed(4))
-    $("#whitePOutput").text(right.a*100)
-    $("#blueMOutput").text(status.blueMultiplier*center.m.toFixed(2))
-    $("#blueVOutput").text(status.blueMultiplier*center.v.toFixed(3))
-    $("#blueKEOutput").text(status.blueMultiplier*(0.5*center.m*(center.v*center.v)).toFixed(4))
-    $("#bluePOutput").text(status.blueMultiplier*center.a*100)
+    $("#whitePOutput").text((right.a*100).toFixed(4))
+    $("#blueMOutput").text((status.blueMultiplier*center.m).toFixed(2))
+    $("#blueVOutput").text((status.blueMultiplier*center.v).toFixed(3))
+    $("#blueKEOutput").text((status.blueMultiplier*(0.5*center.m*(center.v*center.v))).toFixed(4))
+    $("#bluePOutput").text((status.blueMultiplier*(center.a*100)).toFixed(4))
     $("#netMOutput").text((Number($("#redMOutput").text())+Number($("#blueMOutput").text())+Number($("#whiteMOutput").text())).toFixed(2))
     $("#netVOutput").text((Number($("#redVOutput").text())+Number($("#blueVOutput").text())+Number($("#whiteVOutput").text())).toFixed(3))
     $("#netKEOutput").text((Number($("#redKEOutput").text())+Number($("#blueKEOutput").text())+Number($("#whiteKEOutput").text())).toFixed(4))
     $("#netPOutput").text((Number($("#redPOutput").text())+Number($("#bluePOutput").text())+Number($("#whitePOutput").text())).toFixed(4))
-}
-function updateInelastic(a,b){
-    b.v = -b.v
-    if(a.x < b.x){
-        b.x = a.x + a.radius + b.radius + 0.1
-    }else{
-        b.x = a.x - a.radius - b.radius - 0.1
-    }
-}
-function updateInelasticAll(a,b,c,d){
-    if( status.i === 0){
-    if(status.order.length !== 3){
-    if( (a.x > b.x) && (a.x > c.x) ){
-        if(b.x > c.x){
-            status.order=[a, b, c]
-        }else{
-            status.order=[a, c, b]
-        }
-    }
-    else if( (c.x > a.x) && (c.x > b.x) ){
-        if(a.x > b.x){
-            status.order=[c, a, b]
-        }else{
-            status.order=[c, b, a]
-        }
-    }
-    else if( (b.x > a.x) && (b.x > c.x) ){
-        if(a.x > c.x){
-            status.order=[b, a, c]
-        }else{
-            status.order=[b, c, a]
-        }
-    }
-    front = status.order[0]
-    mid = status.order[1]
-    last = status.order[2]
-}
-status.i = 1;
-    }
-    if(a.x > 500){
-        front = status.order[0]
-        mid = status.order[1]
-        last = status.order[2]
-        console.log(front.color +"1a")
-        console.log(mid.color +"2a")
-        console.log(last.color +"3a")
-        front.v = -(Math.abs(d))
-        mid.v = -(Math.abs(d))
-        last.v = -(Math.abs(d))
-        front.x = canvas.width - front.radius - 0.1
-        mid.x = canvas.width - front.radius - front.radius - mid.radius - 0.2
-        last.x = canvas.width - front.radius - front.radius - mid.radius - mid.radius - last.radius - 0.3
-    }else if(a.x < 500){
-        front = status.order[2]
-        mid = status.order[1]
-        last = status.order[0]
-        front.v = Math.abs(d)
-        mid.v = Math.abs(d)
-        last.v = Math.abs(d)
-        last.x = front.radius + front.radius + mid.radius + mid.radius + last.radius + 0.3;
-        mid.x = front.radius + front.radius + mid.radius + 0.2;
-        front.x = front.radius + 0.1;
-    }
 }
 function updateElastic(){
     let leftDirection = (left.v > 0) ? 1 : -1 
@@ -314,35 +162,11 @@ function updateElastic(){
         if(right.x + right.radius > canvas.width){
             right.x = canvas.width - right.radius;
         }
-        else if(right.x - right.radius < 0){
-            right.x = right.radius
-        }
-        if(status.CRL === true){
-            updateInelasticAll(right,center,left,right.v)
-        }
-        else if(status.RL === true){
-            updateInelastic(right,left)
-        }
-        else if(status.CR === true){
-            updateInelastic(right,center)
-        }
     }
     if( left.x + left.radius > canvas.width || left.x - left.radius < 0){
         left.v = - left.v;
         if(left.x + left.radius > canvas.width){
             left.x = canvas.width - left.radius;
-        }
-        else if(left.x - left.radius < 0){
-            left.x = left.radius
-        }
-        if(status.CRL === true){
-            updateInelasticAll(right,center,left,right.v)
-        }
-        if(status.CL === true){
-            updateInelastic(left,center)
-        }
-        else if(status.RL === true){
-             updateInelastic(left,right)
         }
     }
     if( center.x + center.radius > canvas.width || center.x - center.radius < 0){
@@ -350,94 +174,30 @@ function updateElastic(){
         if(center.x + center.radius > canvas.width){
             center.x = canvas.width - center.radius;
         }
-        else if(center.x - center.radius < 0){
-            center.x = center.radius
-        }
-        if(status.CRL === true){
-            updateInelasticAll(right,center,left,right.v)
-        }
-        else if(status.CL === true){
-            updateInelastic(center,left)
-        }
-        else if(status.CR === true){
-            updateInelastic(center,right)
-        } 
     }
     let closeRL = Math.sqrt((right.x - left.x)*(right.x - left.x))
     if(closeRL <= (right.radius+left.radius)){
-        if(status.CL !== true && status.CR !== true){
-            collision(right,left)
-        }
-        else{
-            collisionCOR(right,left,center)
-            collisionCOR(left,right,center)
-        }
-        if(right.v === left.v){
-            status.RL = true;
-            combined.status = true;
-            combinedParticle(right.color,left.color,right.m,left.m,right.v,right.x,left.x)
-            left.combined = true
-            right.combined = true
-        }
+        right.v = (((right.vi*(right.m - left.m))/(right.m + left.m)) + (((2*left.m)/(right.m + left.m))*left.vi))
+        left.v = (((right.vi*(2*right.m))/(right.m + left.m)) + (((left.m - right.m)/(right.m + left.m))*left.vi))
     }
     if(status.blue === true){
         let closeCL = Math.sqrt((center.x - left.x)*(center.x - left.x))
         if(closeCL <= (center.radius+left.radius)){
-            if(status.RL !== true && status.CR !== true){
-            collision(center,left)
-        }
-        else{
-            collisionCOR(center,left,right)
-            collisionCOR(left,center,right)
-        }
-            if(center.v === left.v){
-                status.CL = true;
-                combined.status = true;
-                combinedParticle(center.color,left.color,center.m,left.m,center.v,center.x,left.x)
-                center.combined = true
-                left.combined = true
-            }
+            center.v = (((center.vi*(center.m - left.m))/(center.m + left.m)) + (((2*left.m)/(center.m + left.m))*left.vi))
+            left.v = (((center.vi*(2*center.m))/(center.m + left.m)) + (((left.m - center.m)/(center.m + left.m))*left.vi))
         }
         let closeCR = Math.sqrt((center.x - right.x)*(center.x - right.x))
         if(closeCR <= (center.radius+right.radius)){
-            if(status.RL !== true && status.CL !== true){
-            collision(center,right)
-        }
-        else{
-            collisionCOR(center,right,left)
-            collisionCOR(right,center,left)
-        }
-            if(center.v === right.v){
-            status.CR = true;
-            combined.status = true;
-            combinedParticle(center.color,right.color,center.m,right.m,center.v,center.x,right.x)
-            center.combined = true
-            right.combined = true
-        }
-        }
-    }
-    if((status.RL === true && status.CR === true )||(status.CR === true && status.CL === true )||(status.RL === true && status.CL === true )){
-        status.CRL = true;
-        status.RL = false;
-        status.CR = false;
-        status.CL = false;
-    }
-    if( combined.x + combined.radius > canvas.width || combined.x - combined.radius < 0){
-        combined.v = - combined.v;
-        if(combined.x + combined.radius > canvas.width){
-            combined.x = canvas.width - combined.radius;
-        }
-        else if(combined.x - combined.radius < 0){
-            combined.x = combined.radius
-        }
+            center.v = (((center.vi*(center.m - right.m))/(center.m + right.m)) + (((2*right.m)/(center.m + right.m))*right.vi))
+            right.v = (((center.vi*(2*center.m))/(center.m + right.m)) + (((right.m - center.m)/(center.m + right.m))*right.vi))
+        }   
     }
 }
 function render(){
 drawRect(0, 0, canvas.width, canvas.height, "black");
-drawCircle(left.x, left.y, left.radius, left.color, left.combined)
-drawCircle(right.x, right.y, right.radius, right.color, left.combined)
+drawCircle(left.x, left.y, left.radius, left.color)
+drawCircle(right.x, right.y, right.radius, right.color)
 drawCircleBlue(center.x, center.y, center.radius, center.color, status.blue)
-drawCircleCombined(combined.x, combined.y, combined.radius, combined.color)
 updateText()
 }
 function game(){
@@ -449,19 +209,8 @@ document.addEventListener("mousedown",checkXCoorSliders());
 setInterval(game,);
 $("#restart").click(function(){
     if(status.xCoorClose === false){
-        status.i = 0;
-        status.CR = false;
-        status.CL = false;
-        status.RL = false;
-        status.CRL = false;
-        right.combined = false;
-        left.combined = false;
-        center.combined = false;
-        combined.draw = false;
         status.blue = status.blueCheck
         status.blueMultiplier = status.blueMultiplierCheck
-        status.order = []
-        status.COR = $("#CORRange").val()
         right.x = Number($("#whiteXCoor").val())
         center.x = Number($("#blueXCoor").val())
         left.x = Number($("#redXCoor").val())
@@ -471,9 +220,11 @@ $("#restart").click(function(){
         center.m = Number($("#blueMRangeOutput").text())
         left.v = Number($("#redVRangeOutput").text())
         left.m = Number($("#redMRangeOutput").text())
-        right.radius = 20*Math.sqrt(Number($("#whiteMRangeOutput").text())/Math.PI)+1
-        center.radius = 20*Math.sqrt(Number($("#blueMRangeOutput").text())/Math.PI)+1
-        left.radius = 20*Math.sqrt(Number($("#redMRangeOutput").text())/Math.PI)+1
+        right.radius = 20*Math.sqrt(Number($("#whiteMRangeOutput").text())/Math.PI)
+        center.radius = 20*Math.sqrt(Number($("#blueMRangeOutput").text())/Math.PI)
+        left.radius = 20*Math.sqrt(Number($("#redMRangeOutput").text())/Math.PI)
+        console.log(right.radius)
+        console.log(left.radius)
     }
 })
 var i = 0;
